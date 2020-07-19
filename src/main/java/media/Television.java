a590bd900bdc4c1f477838240f36a65ca537bf91
Dragana <peric.dr@gmail.com>
2020-07-19 20:41:35 +0200

    Added classes Main and Television

---
+++ src/main/java/media/Television.java
@@ -0,0 +1,34 @@
+
+package media;
+
+public class Television {
+    
+    private int volume;
+    private int currentProgram;
+    private boolean turnOn;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getCurrentProgram() {
+        return currentProgram;
+    }
+
+    public void setCurrentProgram(int currentProgram) {
+        this.currentProgram = currentProgram;
+    }
+
+    public boolean isTurnOn() {
+        return turnOn;
+    }
+
+    public void setTurnOn(boolean turnOn) {
+        this.turnOn = turnOn;
+    }
+}
+    
