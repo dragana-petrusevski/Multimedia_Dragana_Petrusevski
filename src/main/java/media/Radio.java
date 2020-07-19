93d8ecbd53d85bdb2525b47e04e950f49220ce6e
Dragana <peric.dr@gmail.com>
2020-07-19 22:07:03 +0200

    Added class Radio

---
+++ src/main/java/media/Radio.java
@@ -0,0 +1,28 @@
+
+package media;
+
+
+public class Radio {
+    private float fmFrequency;
+    private float amFrequency;
+    private char band; 
+
+    public float getFmFrequency() {
+        return fmFrequency;
+    }
+
+    public void setFmFrequency(float fmFrequency) {
+        this.fmFrequency = fmFrequency;
+    }
+
+    public float getAmFrequency() {
+        return amFrequency;
+    }
+
+    public void setAmFrequency(float amFrequency) {
+        this.amFrequency = amFrequency;
+    }
+    
+    
+    
+}
